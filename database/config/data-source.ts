b45cdd068a74b4b9b7d35c8daf0ba80d9994b61b
Sayazhan Onlasyn <sayazhan.onlassyn@mail.ru>
2022-09-23 08:01:35 +0600

    feat: set up database migrations configuration

---
+++ database/config/data-source.ts
@@ -0,0 +1,31 @@
+import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+import {
+  getAppConfig,
+  AppEnvironment,
+} from '../../src/common/config/app.config';
+
+dotenv.config();
+
+const appConfig = getAppConfig();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [],
+  extra:
+    appConfig.environment !== AppEnvironment.Local
+      ? {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        }
+      : undefined,
+  ssl: appConfig.environment !== AppEnvironment.Local,
+  migrations: ['database/migrations/*{.ts,.js}'],
+  synchronize: false,
+});
