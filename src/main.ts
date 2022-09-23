050bf5dd26af48a23d1d0c0823c0cb500965e152
Sayazhan Onlasyn <sayazhan.onlassyn@mail.ru>
2022-09-23 19:08:40 +0600

    feat: configure swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Template API')
+    .setDescription('Documentation of NestJS Template API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(3000);
 }
