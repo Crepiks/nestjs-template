a867a84e64b1109f52ed3e0295bb0448ff62ae49
Sayazhan Onlasyn <sayazhan.onlassyn@mail.ru>
2022-09-23 19:03:45 +0600

    feat: initialize typeorm root module

--- database/config/data-source.ts
+++ database/config/data-source.ts
@@ -4,19 +4,21 @@ import {
   getAppConfig,
   AppEnvironment,
 } from '../../src/common/config/app.config';
+import { getDatabaseConfig } from '../../src/common/config/database.config';
 import { UserDao } from '../../src/common/dao/user.dao';
 
 dotenv.config();
 
 const appConfig = getAppConfig();
+const databaseConfig = getDatabaseConfig();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: databaseConfig.host,
+  port: databaseConfig.port,
+  username: databaseConfig.username,
+  password: databaseConfig.password,
+  database: databaseConfig.database,
   entities: [UserDao],
   extra:
     appConfig.environment !== AppEnvironment.Local
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,6 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppEnvironment, getAppConfig } from 'src/common/config/app.config';
+import { getDatabaseConfig } from 'src/common/config/database.config';
+import { UserDao } from 'src/common/dao/user.dao';
+
+const appConfig = getAppConfig();
+const databaseConfig = getDatabaseConfig();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: databaseConfig.host,
+      port: databaseConfig.port,
+      username: databaseConfig.username,
+      password: databaseConfig.password,
+      database: databaseConfig.database,
+      entities: [UserDao],
+      extra:
+        appConfig.environment !== AppEnvironment.Local
+          ? {
+              ssl: {
+                rejectUnauthorized: false,
+              },
+            }
+          : undefined,
+      ssl: appConfig.environment !== AppEnvironment.Local,
+      synchronize: false,
+    }),
+  ],
 })
 export class AppModule {}
